be59a73872daa8c7f3ae5f2ad1409661b61719ef
Erin Weir <erinlmaestas@gmail.com>
2018-08-30 19:45:06 -0700

    Remove unused routes that are being replaced with React/javascript redirects.

--- hello.py
+++ hello.py
@@ -9,17 +9,12 @@ def index():
     return render_template('Question1React.html')
 
 
-@app.route('/nextpage', methods=['GET'])
-def next_Page():
-    return redirect(url_for('question2'))
-
-
 @app.route('/posttest', methods=['GET', 'POST'])
 def post_Test():
     identifier = str(request.get_json()['identifier'])
     user_id = p.insert_data(identifier, request.get_json()[identifier])
     print(str(user_id) + ' returned')
-    return user_id
+    return 'ok'
 
 
 @app.route('/updatedata', methods=['GET', 'POST'])
@@ -28,13 +23,3 @@ def update_Data():
     user_id = request.get_json()['userID']
     p.update_data(user_id, identifier, request.get_json()[identifier])
     return 'ok'
-
-
-@app.route('/question2', methods=['GET', 'POST'])
-def question2():
-    return render_template('Question2.html')
-
-
-@app.route('/question3', methods=['GET', 'POST'])
-def question():
-    return render_template('Question3.html')
